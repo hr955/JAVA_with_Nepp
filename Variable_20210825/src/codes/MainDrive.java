d455c7f06599160b21d412839d02093927f5632f
hr955 <nfzoze01@gmail.com>
2021-08-25 17:20:45 +0900

    create Variable_20210825

---
+++ Variable_20210825/src/codes/MainDrive.java
@@ -0,0 +1,18 @@
+package codes;
+
+public class MainDrive {
+
+	public static void main(String[] args) {
+
+		int myAge = 13;
+		String myName = "¿Âµ∂¥Î";
+		double myHeight = 188.7;
+		boolean isMale = false;
+		
+		System.out.println(myAge);
+		System.out.println(myName);
+		System.out.println(myHeight);
+		System.out.println(isMale);
+
+	}
+}
