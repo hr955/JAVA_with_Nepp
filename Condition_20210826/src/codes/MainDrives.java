d8754e9d92eae77d7a1b9b202892e1214d02b82e
hr955 <nfzoze01@gmail.com>
2021-08-26 12:50:59 +0900

    Scanner 예제

--- Condition_20210826/src/codes/MainDrives.java
+++ Condition_20210826/src/codes/MainDrives.java
@@ -1,16 +1,22 @@
 package codes;
 
+import java.util.Scanner;
+
 public class MainDrives {
 	public static void main(String[] args) {
-
-		int userAge = 17;
-
+		
+		Scanner myScanner = new Scanner(System.in);
+		
+		System.out.print("나이 입력 : ");
+		
+		int userAge = myScanner.nextInt();
+		
 		if (userAge >= 20) {
-			System.out.println("판매 ok");
+			System.out.println("성인");
 		} else if (userAge >= 17) {
 			System.out.println("고등학생");
 		} else {
-			System.out.println("판매 불가");
+			System.out.println("미성년자");
 		}
 
 	}
