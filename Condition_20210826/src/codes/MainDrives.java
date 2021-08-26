c00b60942264f0e006eed270f11aa7fa8e0e2203
hr955 <nfzoze01@gmail.com>
2021-08-26 12:44:59 +0900

    create Condition_20210826

---
+++ Condition_20210826/src/codes/MainDrives.java
@@ -0,0 +1,17 @@
+package codes;
+
+public class MainDrives {
+	public static void main(String[] args) {
+
+		int userAge = 17;
+
+		if (userAge >= 20) {
+			System.out.println("판매 ok");
+		} else if (userAge >= 17) {
+			System.out.println("고등학생");
+		} else {
+			System.out.println("판매 불가");
+		}
+
+	}
+}
