93a9a67c43243518d13c12e2109e5dda2be852ba
hr955 <nfzoze01@gmail.com>
2021-08-26 10:08:34 +0900

    변수 응용문제

---
+++ Variable_20210825/src/codes/VariablePractice.java
@@ -0,0 +1,13 @@
+package codes;
+
+public class VariablePractice {
+	public static void main(String[] args) {
+		String myName = "Æ©ºê";
+		int myAge = 5;
+		
+		System.out.println(myName);
+		System.out.println(myAge);
+		
+	}
+
+}
---
+++ Variable_20210825/src/codes/VariablePractice2.java
@@ -0,0 +1,19 @@
+package codes;
+
+public class VariablePractice2 {
+	public static void main(String[] args) {
+		
+		
+		//Q. num1과 num2의 값을 서로 바꾸기
+		int num1 = 50;
+		int num2 = 20;
+		
+		int temp = num1;
+		num1 = num2;
+		num2 = temp;
+		
+		System.out.println(num1);
+		System.out.println(num2);
+		
+	}
+}
