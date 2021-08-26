6f14c58cc243f93498015f9f20b05880c4d2b910
hr955 <nfzoze01@gmail.com>
2021-08-26 15:01:14 +0900

    학점 계산 문제

---
+++ Condition_20210826/src/codes/Condition_Grade.java
@@ -0,0 +1,41 @@
+package codes;
+
+import java.util.Scanner;
+
+public class Condition_Grade {
+	public static void main(String[] args) {
+
+		// 학점부여기준 : 90점 이상(추가검사) => 5등 이내 : A+
+		// 90점 이상 -> 6등이하 : A0
+		// 80점 이상 -> B
+		// 70점 이상 -> C
+		// 그 외 -> F
+
+		Scanner scanner = new Scanner(System.in);
+
+		System.out.print("점수 입력 : ");
+		int score = scanner.nextInt();
+
+		// 등수 입력
+		System.out.print("등수 입력 : ");
+		int rank = scanner.nextInt();
+
+		// 90점 이상인가?
+		if (score >= 90) {
+			// 등수가 5등 이상인가?
+			if (rank <= 5) {
+				// 점수가 90점 이상이고 등수가 5등 이내라면 A+
+				System.out.println("A+");
+			} else {
+				System.out.println("A0");
+			}
+
+		} else if (score >= 80) {
+			System.out.println("B");
+		} else if (score >= 70) {
+			System.out.println("C");
+		} else {
+			System.out.println("F");
+		}
+	}
+}
