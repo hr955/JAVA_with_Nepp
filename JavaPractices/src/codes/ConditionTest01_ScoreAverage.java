68326345de9c52c3f24fce413b2cc51dd30ba101
hr955 <nfzoze01@gmail.com>
2021-08-26 15:41:37 +0900

    과락 추가

--- JavaPractices/src/codes/ConditionTest01_ScoreAverage.java
+++ JavaPractices/src/codes/ConditionTest01_ScoreAverage.java
@@ -12,13 +12,18 @@ public class ConditionTest01_ScoreAverage {
 		int mathScore = scanner.nextInt();
 		System.out.print("영어 점수 : ");
 		int englishScore = scanner.nextInt();
-		
+
 		scanner.close();
 
 		int avg = (koreanScore + mathScore + englishScore) / 3;
 
-		if (avg >= 60 && koreanScore >= 40 && mathScore >= 40 && englishScore >= 40) {
-			System.out.println("합격");
+		if (avg >= 60) {
+			if (koreanScore < 40 || mathScore < 40 || englishScore < 40) {
+				System.out.println("불합격(과락)");
+
+			} else {
+				System.out.println("합격");
+			}
 		} else {
 			System.out.println("불합격");
 		}
