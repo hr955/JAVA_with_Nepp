b714f12c4e80a134412cbd6a563652e1c648b2f5
hr955 <nfzoze01@gmail.com>
2021-08-26 10:59:27 +0900

    연산자 연습문제

---
+++ Operator01_20210826/src/codes/OperatorPractice.java
@@ -0,0 +1,11 @@
+package codes;
+
+public class OperatorPractice {
+	public static void main(String[] args) {
+
+		int myBirthYear = 2000;
+		int myKoreaAge = 2021 - myBirthYear + 1;
+		System.out.println(myBirthYear + "³â»ý-" + myKoreaAge + "¼¼");
+
+	}
+}
---
+++ Operator01_20210826/src/codes/OperatorPractice2.java
@@ -0,0 +1,12 @@
+package codes;
+
+public class OperatorPractice2 {
+	public static void main(String[] args) {
+		
+		double startMoney = 100000;
+		double nowMoney = 115000;
+		double rate = (nowMoney - startMoney) / startMoney * 100;
+
+		System.out.println("¼öÀÍ·ü = " + rate + "%");
+	}
+}
