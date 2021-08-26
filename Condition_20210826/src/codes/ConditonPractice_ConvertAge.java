0c1f6ca7ef4dd8cb50d760694cb0757811dd01a8
hr955 <nfzoze01@gmail.com>
2021-08-26 14:45:43 +0900

    주석추가

--- Condition_20210826/src/codes/ConditonPractice_ConvertAge.java
+++ Condition_20210826/src/codes/ConditonPractice_ConvertAge.java
@@ -4,15 +4,22 @@ import java.util.Scanner;
 
 public class ConditonPractice_ConvertAge {
 
+	//성인, 미성년자 판별
+	
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 		System.out.print("출생년도 입력 : ");
+		
+		//출생년도 사용자 입력
 		int birthYear = scanner.nextInt();
-
+		
+		//현재 나이
 		int age = 2021 - birthYear + 1;
 
+		//성인,미성년자 판별
 		String result = age >= 20 ? "성인" : "미성년자";
 
+		//출력
 		System.out.println(result);
 	}
 
