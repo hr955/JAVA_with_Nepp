9597240dc4218816341a8e81d4a6920467c31a07
hr955 <nfzoze01@gmail.com>
2021-08-27 10:41:07 +0900

    for문 예제

---
+++ Repeat_20210827/src/codes/Repeat_For.java
@@ -0,0 +1,36 @@
+package codes;
+
+public class Repeat_For {
+	public static void main(String[] args) {
+		for (int i = 1; i <= 10; i++) {
+			System.out.println(i);
+		}
+
+		System.out.println("=================");
+
+		for (int i = 20; i >= 11; i--) {
+			System.out.println(i);
+		}
+
+		System.out.println("=================");
+
+		for (int i = 0; i < 5; i++) {
+			System.out.println("안녕하세요");
+
+		}
+
+		System.out.println("=================");
+
+		for (int i = 3; i <= 30; i += 3) {
+			System.out.println(i);
+		}
+
+		System.out.println("=================");
+
+		for (int i = 1; i < 10; i++) {
+			System.out.println("2 * " + i + " = " + 2 * i);
+		}
+
+	}
+
+}
